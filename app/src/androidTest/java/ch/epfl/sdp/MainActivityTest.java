175adf30e536faa18c18eae158bd55bcec42a7cd
matteopariset <matteopariset@gmail.com>
2020-03-01 02:18:02 +0100

    Adding tests

---
+++ app/src/androidTest/java/ch/epfl/sdp/MainActivityTest.java
@@ -0,0 +1,26 @@
+package ch.epfl.sdp;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public final ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void firstTest() {
+        onView(withId(R.id.mainName)).perform(typeText("Someone changed me")).perform(closeSoftKeyboard());
+        onView(withId(R.id.mainGo)).perform(click());
+    }
+}
